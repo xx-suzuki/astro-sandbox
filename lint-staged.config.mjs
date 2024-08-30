d254e074e71310402e8f86b99a533300a83b0eba
suzuki <username@users.noreply.github.com>
2024-08-30 18:39:39 +0900

    fix: lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,7 +3,11 @@ export default {
     "eslint -c eslint.config.mjs --fix --ignore-pattern '!.*rc.cjs'",
     'prettier --write --no-error-on-unmatched-pattern',
   ],
-  './src/**/*.{ts,tsx,astro,vue}': [
+  './script/task/**/*.{js,cjs,mjs,ts}': [
+    "eslint -c eslint.config.mjs --fix --ignore-pattern '!.*rc.cjs'",
+    'prettier --write --no-error-on-unmatched-pattern',
+  ],
+  './src/**/*.{ts,tsx,astro}': [
     'eslint -c eslint.config.mjs --fix',
     'prettier --write --no-error-on-unmatched-pattern',
   ],
