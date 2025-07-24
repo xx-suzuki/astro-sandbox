f554868f9cefc94aa44353ca8fc290a693b77fd8
suzuki <hideshi.suzuki@lineaworks.jp>
2025-07-24 12:06:20 +0900

    feature: updated markuplint settings

--- markuplint.config.ts
+++ markuplint.config.ts
@@ -1,10 +1,29 @@
 import type { Config } from '@markuplint/ml-config';
 
 const config: Config = {
-  // parser: {
-  //   '\\.astro$': '@markuplint/astro-parser',
-  // },
+  parser: {
+    '\\.astro$': '@markuplint/astro-parser',
+  },
+  parserOptions: {
+    ignoreFrontMatter: true,
+  },
   extends: ['markuplint:recommended'],
+  rules: {
+    'invalid-attr': {
+      options: {
+        allowAttrs: ['prefix'],
+      },
+    },
+  },
+  pretenders: [
+    {
+      selector: 'source',
+      as: {
+        element: 'source',
+        inheritAttrs: true,
+      },
+    },
+  ],
   nodeRules: [
     {
       selector: 'svg',
